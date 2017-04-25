f594d1d720805f51bf98731c3551d46f79e36ea3
Zack <zholloway92@gmail.com>
2017-04-25 18:40:05 -0400

    commit

---
+++ Marble/Controllers/MarbleController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Marble.Controllers
+{
+    public class MarbleController : ApiController
+    {
+        public class Marble
+        {
+            public string Color { get; set; }
+        }
+
+        List<Marble> marbleBag = new List<Marble>();
+
+        // GET: Marble
+        public IHttpActionResult Get()
+        {
+            var rnd = new Random();
+            int random = rnd.Next(0, (marbleBag.Count - 1));
+
+            return Ok(marbleBag[random]);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Add(Marble marble)
+        {
+            marbleBag.Add(marble);
+            
+            return Ok();
+        }
+    }
+}
---
+++ Marble/Scripts/Marble.js
@@ -0,0 +1,42 @@
+﻿let addToList = (data) => {
+    $("#marbleList")
+        .append($("<li>").html(data.Name))
+}
+
+let addToListTwo = (data) => {
+    $("#marbleList2")
+        .append($("<li>").html(data.Name))
+}
+
+let GetRandomMarble = () => {
+    $.ajax({
+        url: '/api/Marble',
+        dataType: "json",
+        success: (data) => {
+            data.map((item) => { addToList(item); });
+        }
+    });
+}
+
+let AddMarble = () => {
+    let newMarble = {
+        Color: $("#newMarbleColor").val()
+    };
+
+    $.ajax({
+        url: '/api/Marble',
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify(newMarble),
+        type: "POST",
+        success: (data) => {
+            addToListTwo(data);
+        },
+        error: (data) => {
+            console.log("oops", data)
+        },
+        complete: (data) => {
+            console.log("done", data);
+        }
+    });
+}
